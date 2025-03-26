f0670cf1dc923f8ae9de7b507c06cc0b13cd1806
Meena Nadeem <meenandm@PK.local>
2025-03-26 11:22:36 +0000

    output measurements cpu vs cuda

---
+++ examples/run/cuda/cluster_example_cuda.cpp
@@ -0,0 +1,257 @@
+/** TRACCC library, part of the ACTS project (R&D line)
+ *
+ * (c) 2021-2024 CERN for the benefit of the ACTS project
+ *
+ * Mozilla Public License Version 2.0
+ */
+
+// Project include(s).
+#include "traccc/clusterization/clusterization_algorithm.hpp"
+#include "traccc/cuda/clusterization/clusterization_algorithm.hpp"
+#include "traccc/cuda/clusterization/measurement_sorting_algorithm.hpp"
+#include "traccc/cuda/utils/stream.hpp"
+#include "traccc/device/container_d2h_copy_alg.hpp"
+#include "traccc/io/read_cells.hpp"
+#include "traccc/io/read_detector.hpp"
+#include "traccc/io/read_detector_description.hpp"
+#include "traccc/io/utils.hpp"
+#include "traccc/io/write.hpp"
+#include "traccc/options/accelerator.hpp"
+#include "traccc/options/clusterization.hpp"
+#include "traccc/options/detector.hpp"
+#include "traccc/options/input_data.hpp"
+#include "traccc/options/output_data.hpp"
+#include "traccc/options/performance.hpp"
+#include "traccc/options/program_options.hpp"
+#include "traccc/performance/collection_comparator.hpp"
+#include "traccc/performance/container_comparator.hpp"
+#include "traccc/performance/timer.hpp"
+
+// Detray include(s).
+#include "detray/core/detector.hpp"
+#include "detray/detectors/bfield.hpp"
+#include "detray/io/frontend/detector_reader.hpp"
+#include "detray/navigation/navigator.hpp"
+#include "detray/propagator/propagator.hpp"
+#include "detray/propagator/rk_stepper.hpp"
+
+// VecMem include(s).
+#include <vecmem/memory/cuda/device_memory_resource.hpp>
+#include <vecmem/memory/cuda/host_memory_resource.hpp>
+#include <vecmem/memory/host_memory_resource.hpp>
+#include <vecmem/utils/cuda/async_copy.hpp>
+
+// System include(s).
+#include <exception>
+#include <iomanip>
+#include <iostream>
+#include <memory>
+
+int clustering_run(const traccc::opts::detector& detector_opts,
+            const traccc::opts::input_data& input_opts,
+            const traccc::opts::output_data& output_opts,
+            const traccc::opts::clusterization& clusterization_opts,
+            const traccc::opts::performance& performance_opts,
+            const traccc::opts::accelerator& accelerator_opts) {
+
+    // Memory resources used by the application.
+    vecmem::host_memory_resource host_mr;
+    vecmem::cuda::host_memory_resource cuda_host_mr;
+    vecmem::cuda::device_memory_resource device_mr;
+    traccc::memory_resource mr{device_mr, &cuda_host_mr};
+
+    // CUDA types used.
+    traccc::cuda::stream stream;
+    vecmem::cuda::async_copy copy{stream.cudaStream()};
+
+    // Construct the detector description object.
+    traccc::silicon_detector_description::host host_det_descr{host_mr};
+    traccc::io::read_detector_description(
+        host_det_descr, detector_opts.detector_file,
+        detector_opts.digitization_file,
+        (detector_opts.use_detray_detector ? traccc::data_format::json
+                                           : traccc::data_format::csv));
+    traccc::silicon_detector_description::data host_det_descr_data{
+        vecmem::get_data(host_det_descr)};
+    traccc::silicon_detector_description::buffer device_det_descr{
+        static_cast<traccc::silicon_detector_description::buffer::size_type>(
+            host_det_descr.size()),
+        device_mr};
+    copy.setup(device_det_descr)->wait();
+    copy(host_det_descr_data, device_det_descr)->wait();
+
+    // Construct a Detray detector object, if supported by the configuration.
+    traccc::default_detector::host host_detector{host_mr};
+    traccc::default_detector::buffer device_detector;
+    traccc::default_detector::view device_detector_view;
+    if (detector_opts.use_detray_detector) {
+        traccc::io::read_detector(
+            host_detector, host_mr, detector_opts.detector_file,
+            detector_opts.material_file, detector_opts.grid_file);
+        device_detector = detray::get_buffer(detray::get_data(host_detector),
+                                             device_mr, copy);
+        stream.synchronize();
+        device_detector_view = detray::get_data(device_detector);
+    }
+
+    // Output stats
+    uint64_t n_cells = 0;
+    uint64_t n_measurements = 0;
+    uint64_t n_measurements_cuda = 0;
+
+
+  
+    traccc::host::clusterization_algorithm ca(host_mr);
+    
+    traccc::cuda::clusterization_algorithm ca_cuda(mr, copy, stream,
+                                                   clusterization_opts);
+
+    traccc::cuda::measurement_sorting_algorithm ms_cuda(copy, stream);
+    
+    // performance writer (for cluster and measurement)
+//    traccc::seeding_performance_writer sd_performance_writer(
+//        traccc::seeding_performance_writer::config{});
+
+    traccc::performance::timing_info elapsedTimes;
+
+    // Loop over events
+    for (std::size_t event = input_opts.skip;
+         event < input_opts.events + input_opts.skip; ++event) {
+
+        // Instantiate host containers/collections
+        traccc::host::clusterization_algorithm::output_type
+            measurements_per_event;
+       
+        // Instantiate cuda containers/collections
+        traccc::measurement_collection_types::buffer measurements_cuda_buffer(
+            0, *mr.host);                
+        {
+            traccc::performance::timer wall_t("Wall time", elapsedTimes);
+
+            traccc::edm::silicon_cell_collection::host cells_per_event{host_mr};
+
+            {
+                traccc::performance::timer t("File reading  (cpu)",
+                                             elapsedTimes);
+                // Read the cells from the relevant event file into host memory.
+                static constexpr bool DEDUPLICATE = true;
+                traccc::io::read_cells(cells_per_event, event,
+                                       input_opts.directory, &host_det_descr,
+                                       input_opts.format, DEDUPLICATE,
+                                       input_opts.use_acts_geom_source);
+            }  // stop measuring file reading timer
+
+            n_cells += cells_per_event.size();
+
+            // Create device copy of input collections
+            traccc::edm::silicon_cell_collection::buffer cells_buffer(
+                static_cast<unsigned int>(cells_per_event.size()), mr.main);
+            copy.setup(cells_buffer)->wait();
+            copy(vecmem::get_data(cells_per_event), cells_buffer)->wait();
+
+            // CUDA
+            {
+                traccc::performance::timer t("Clusterization (cuda)",
+                                             elapsedTimes);
+                // Reconstruct it into spacepoints on the device.
+                measurements_cuda_buffer =
+                    ca_cuda(cells_buffer, device_det_descr);
+                ms_cuda(measurements_cuda_buffer);
+                stream.synchronize();
+            }  // stop measuring clusterization cuda timer
+
+            // CPU
+            if (accelerator_opts.compare_with_cpu) {
+                traccc::performance::timer t("Clusterization  (cpu)",
+                                             elapsedTimes);
+                measurements_per_event =
+                    ca(vecmem::get_data(cells_per_event), host_det_descr_data);
+            }  // stop measuring clusterization cpu timer
+
+            // Perform seeding, track finding and fitting only when using a
+            // Detray geometry.
+            if (detector_opts.use_detray_detector) {
+
+         
+            }
+
+        }  // Stop measuring wall time
+
+        /*----------------------------------
+          compare cpu and cuda result
+          ----------------------------------*/
+
+        traccc::measurement_collection_types::host measurements_per_event_cuda;
+        
+        copy(measurements_cuda_buffer, measurements_per_event_cuda)->wait();
+        
+        stream.synchronize();
+                     
+        if (output_opts.directory != "") {
+            traccc::io::write(event, output_opts.directory, 
+                                output_opts.format,
+                                vecmem::get_data(measurements_per_event_cuda));
+        }
+
+        if (accelerator_opts.compare_with_cpu) {
+
+            // Show which event we are currently presenting the results for.
+            std::cout << "===>>> Event " << event << " <<<===" << std::endl;
+
+            // Compare the measurements made on the host and on the device.
+            traccc::collection_comparator<traccc::measurement>
+                compare_measurements{"measurements"};
+            compare_measurements(vecmem::get_data(measurements_per_event),
+                                 vecmem::get_data(measurements_per_event_cuda));
+
+                      
+        }
+
+        /// Statistics
+        n_measurements += measurements_per_event.size();
+        n_measurements_cuda += measurements_per_event_cuda.size();
+
+        if (performance_opts.run) {
+
+            // TODO: Do evt_data.fill_cca_result(...) with cuda clusters and
+            // measurements
+        }
+    }
+
+ //   if (performance_opts.run) {
+ //       sd_performance_writer.finalize();
+ //   }
+
+    std::cout << "==> Statistics ... " << std::endl;
+    std::cout << "- read    " << n_cells << " cells" << std::endl;
+    std::cout << "- created (cpu)  " << n_measurements << " measurements     "
+              << std::endl;
+    std::cout << "- created (cuda)  " << n_measurements_cuda
+              << " measurements     " << std::endl;
+   
+    std::cout << "==>Elapsed times...\n" << elapsedTimes << std::endl;
+
+    return 0;
+		}
+	
+// The main routine
+//
+int main(int argc, char* argv[]) {
+
+    // Program options.
+    traccc::opts::detector detector_opts;
+    traccc::opts::input_data input_opts;
+    traccc::opts::output_data output_opts{traccc::data_format::obj, ""};
+    traccc::opts::clusterization clusterization_opts;
+    traccc::opts::performance performance_opts;
+    traccc::opts::accelerator accelerator_opts;
+    traccc::opts::program_options program_opts{
+        "Full Tracking Chain Using CUDA",
+        {detector_opts, input_opts, output_opts, clusterization_opts, performance_opts, accelerator_opts},
+        argc,
+        argv};
+
+    // Run the application.
+    return clustering_run(detector_opts, input_opts, output_opts, clusterization_opts, performance_opts,
+                   accelerator_opts);
+}
